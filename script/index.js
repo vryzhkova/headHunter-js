aa0d2591f5c3a90562b7e615d1ffffe7d3ec5f40
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-03-24 16:03:15 +0400

    added lesson 2

--- script/index.js
+++ script/index.js
@@ -62,9 +62,13 @@ cityRegionList.addEventListener('click', (event) => {
 
     if(target.classList.contains('city__link')) {
         topCityBtn.textContent = target.textContent;
-        city.classList.remove('city_active')
+        city.classList.remove('city_active');
     }
 });
+
+cityClose.addEventListener('click', () => {
+    city.classList.remove('city_active');
+})
 
 // modal window
 
@@ -88,3 +92,86 @@ overlayVacancy.addEventListener('click', (e) => {
     
 })
 
+
+//вывод карточек
+
+const createCard = (vacancy) => {
+
+    const {
+        title, 
+        id, 
+        compensation, 
+        workSchedule, 
+        employer,
+        address,
+        description,
+        date
+        } = vacancy;
+
+    const card = document.createElement('li');
+    card.classList.add('result__item');
+
+    card.insertAdjacentHTML('afterbegin', `
+            <article class="vacancy">
+                <h2 class="vacancy__title">
+                    <a class="vacancy__open-modal" href="#" data-vacancy="${id}">${title}</a>
+                </h2>
+                <p class="vacancy__compensation">${compensation}</p>
+                <p class="vacancy__work-schedule">${workSchedule}</p>
+                <div class="vacancy__employer">
+                    <p class="vacancy__employer-title">${employer}</p>
+                    <p class="vacancy__employer-address">${address}</p>
+                </div>
+                <p class="vacancy__description">${description}</p>
+                <p class="vacancy__date">
+                    <time datetime="${date}">${date}</time>
+                </p>
+                <div class="vacancy__wrapper-btn">
+                    <a class="vacancy__response vacancy__open-modal" href="#" data-vacancy="${id}">Откликнуться</a>
+                    <button class="vacancy__contacts">Показать контакты</button>
+                </div>
+            </article>
+    `);
+
+    return card;
+};
+
+const renderCards = (data) => {
+    resultList.textContent = '';
+    const cards = data.map(createCard);
+    resultList.append(...cards);
+};
+
+const getData = ({search} = {}) => {
+    if(search) {
+        return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
+    }
+
+    return fetch('http://localhost:3000/api/vacancy').then(response => response.json());
+};
+
+const formSearch = document.querySelector('.bottom__search');
+formSearch.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const textSearch = formSearch.search.value;
+
+    if(textSearch.length > 2) {
+        formSearch.search.style.borderColor = '';
+
+        const data = await getData({search: textSearch});
+        renderCards(data);
+        formSearch.reset();
+    } else {
+        formSearch.search.style.borderColor = 'red';
+        setTimeout(() => {
+            formSearch.search.style.borderColor = '';
+        }, 2000)
+    }
+})
+
+const init = async () => {
+    const data = await getData();
+    renderCards(data);
+};
+
+init();
