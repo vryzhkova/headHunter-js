b026f12b6b3373ba3b3ac9d5a710048cdf436960
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-03-28 13:00:02 +0400

    ended lesson 4

--- script/index.js
+++ script/index.js
@@ -12,13 +12,26 @@ const overlayVacancy = document.querySelector('.overlay_vacancy');
 const resultList = document.querySelector('.result__list');
 const formSearch = document.querySelector('.bottom__search');
 const found = document.querySelector('.found');
+const orderBy = document.getElementById('order_by');
+const searchPeriod = document.getElementById('search_period');
 
-const getData = ({search, id} = {}) => {
+let data = [];
+
+const getData = ({search, id, country, city} = {}) => {
+    let url = `http://localhost:3000/api/vacancy/${id ? id : ''}`;
     if(search) {
-        return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
+        url = `http://localhost:3000/api/vacancy?search=${search}`;
     }
 
-    return fetch(`http://localhost:3000/api/vacancy/${id ? id : ''}`).then(response => response.json());
+    if(city) {
+        url = `http://localhost:3000/api/vacancy?city=${city}`
+    }
+
+    if(country) {
+        url = `http://localhost:3000/api/vacancy?country=${country}`
+    }
+
+    return fetch(url).then(response => response.json());
 };
 
 
@@ -72,8 +85,26 @@ const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11
         const cards = data.map(createCard);
         resultList.append(...cards);
     };
-    
 
+    const sortData = () => {
+        switch (orderBy.value) {
+            case 'down':
+                data.sort((a, b) => a.minCompensation > b.minCompensation ? 1 : -1);
+                break
+            case 'up':
+                data.sort((a, b) => b.minCompensation > a.minCompensation ? 1 : -1);
+                break;
+            default:
+                data.sort((a, b) => new Date(a.date).getTime() > new Date(b.date).getTime() ? 1 : -1);
+        }
+    };
+
+    const filterData = () => {
+        const date = new Date();
+        date.setDate(date.getDate() - searchPeriod.value);
+        return data.filter(item => new Date(item.date).getTime() > date);
+    }
+    
     const optionHandler = () => {
         optionBtnOrder.addEventListener('click', () => {
             optionListOrder.classList.toggle('option__list_active');
@@ -90,6 +121,9 @@ const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11
         
             if(target.classList.contains('option__item')) {
                 optionBtnOrder.textContent = target.textContent;
+                orderBy.value = target.dataset.sort;
+                sortData();
+                renderCards(data);
                 optionListOrder.classList.remove('option__list_active')
                 for(const elem of optionListOrder.querySelectorAll('.option__item')) {
                     if(elem === target) {
@@ -106,6 +140,9 @@ const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11
         
             if(target.classList.contains('option__item')) {
                 optionBtnPeriod.textContent = target.textContent;
+                searchPeriod.value = target.dataset.date;
+                const tempData = filterData();
+                renderCards(tempData);
                 optionListPeriod.classList.remove('option__list_active')
                 for(const elem of optionListPeriod.querySelectorAll('.option__item')) {
                     if(elem === target) {
@@ -125,10 +162,18 @@ const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11
                 city.classList.toggle('city_active');
             })
             
-            cityRegionList.addEventListener('click', (event) => {
+            cityRegionList.addEventListener('click', async (event) => {
                 const target = event.target;
             
                 if(target.classList.contains('city__link')) {
+                    // console.log(target.getAttribute('href').substring(1));
+                    const hash = new URL(target.href).hash.substring(1);
+                    const option = {
+                        [hash]: target.textContent,
+                    }
+                    data = await getData(option);
+                    sortData();
+                    renderCards(data);
                     topCityBtn.textContent = target.textContent;
                     city.classList.remove('city_active');
                 }
@@ -257,7 +302,8 @@ const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11
         
             if(textSearch.length > 2) {
                 formSearch.search.style.borderColor = '';
-                const data = await getData({search: textSearch});
+                data = await getData({search: textSearch});
+                sortData();
                 renderCards(data);
                 found.innerHTML = `${declOfNum(data.length, ['вакансия', 'вакансии', 'вакансий'])} &laquo;${textSearch}&raquo;`;
                 formSearch.reset();
@@ -273,8 +319,11 @@ const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11
 
 
     const init = async () => {
-        const data = await getData();
+        data = await getData();
+        sortData();
+        data = filterData();
         renderCards(data);
+        
         optionHandler();
         cityHandler();
         modalHandler();
